c554fb9a013eb114768083721b50096c8058a292
HaleOIC <shinehale730@gmail.com>
2024-12-25 17:06:04 +0100

    feat: add control flow instructions

--- src/compiler.c
+++ src/compiler.c
@@ -1,5 +1,15 @@
 #include "feeny/compiler.h"
 
+// #define DEBUG 1
+
+static int label_counter = 0;
+
+static char *genLabel() {
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "L%d", label_counter++);
+    return strdup(buffer);
+}
+
 FuncContext *newFuncContext() {
     FuncContext *context = (FuncContext *)malloc(sizeof(FuncContext));
     context->instructions = make_vector();
@@ -16,6 +26,7 @@ ObjContext *newObjContext() {
     context->names = make_vector();
     context->slots = make_vector();
     context->prev = NULL;
+    return context;
 }
 
 Value *newNullValue() {
@@ -40,6 +51,7 @@ StringValue *newStringValue(char *v) {
 
 MethodValue *newMethodValue(int name, int nargs, int nlocals, Vector *code) {
     MethodValue *rv = (MethodValue *)malloc(sizeof(MethodValue));
+    rv->tag = METHOD_VAL;
     rv->code = code;
     rv->name = name;
     rv->nargs = nargs;
@@ -61,6 +73,20 @@ ClassValue *newClassValue(Vector *slots) {
     return rv;
 }
 
+void addNullInstr(CompileInfo *info) {
+    int nullIndex = addConstantValue(info->pool, (Value *)newNullValue());
+    LitIns *nullInstr = (LitIns *)malloc(sizeof(LitIns));
+    nullInstr->tag = LIT_OP;
+    nullInstr->idx = nullIndex;
+    vector_add(info->funcContext->instructions, nullInstr);
+}
+
+void addReturnInstr(CompileInfo *info) {
+    ByteIns *return_ins = (ByteIns *)malloc(sizeof(ByteIns));
+    return_ins->tag = RETURN_OP;
+    vector_add(info->funcContext->instructions, return_ins);
+}
+
 /* Assumption: Vector only contains int */
 static int compare_vectors(Vector *v1, Vector *v2) {
     if (v1->size != v2->size) {
@@ -77,12 +103,16 @@ static int compare_vectors(Vector *v1, Vector *v2) {
     return 0;
 }
 
-int compare(Value *v1, Value *v2) {
+static int compare(Value *v1, Value *v2) {
     if (v1->tag != v2->tag) {
         return v1->tag - v2->tag;
     }
 
     switch (v1->tag) {
+    case NULL_VAL: {
+        return 0;
+    }
+
     case INT_VAL: {
         IntValue *i1 = (IntValue *)v1;
         IntValue *i2 = (IntValue *)v2;
@@ -155,7 +185,326 @@ static int findIndexByName(Vector *pool, Value *value) {
     return -1;
 }
 
+static int findLocalVar(FuncContext *context, char *name) {
+    for (int i = 0; i < context->nargs; i++) {
+        if (strcmp((char *)vector_get(context->args, i), name) == 0) {
+            return i;
+        }
+    }
+    for (int i = 0; i < context->nlocals; i++) {
+        if (strcmp((char *)vector_get(context->locals, i), name) == 0) {
+            return i + context->nargs;
+        }
+    }
+
+    return -1;
+}
+
+static VarLocation findObjVar(ObjContext *context, char *name) {
+    VarLocation location;
+    location.index = -1;
+
+    for (int i = 0; i < vector_size(context->names); i++) {
+        if (strcmp((char *)vector_get(context->names, i), name) == 0) {
+            location.index = (intptr_t)vector_get(context->slots, i);
+            location.type = (context->prev == NULL) ? GLOBAL_VAR : SLOT_VAR;
+            return location;
+        }
+    }
+
+    if (context->prev != NULL) {
+        return findObjVar(context->prev, name);
+    }
+
+    return location;
+}
+
+static void compileLiteral(CompileInfo *info, Value *value) {
+    int val_idx = addConstantValue(info->pool, value);
+    LitIns *lit = (LitIns *)malloc(sizeof(LitIns));
+    lit->tag = LIT_OP;
+    lit->idx = val_idx;
+    vector_add(info->funcContext->instructions, lit);
+}
+
+static void compilePrintf(CompileInfo *info, PrintfExp *printf_exp) {
+    for (int i = 0; i < printf_exp->nexps; i++) {
+        compileExpr(info, printf_exp->exps[i]);
+    }
+
+    StringValue *format = newStringValue(strdup(printf_exp->format));
+    int format_idx = addConstantValue(info->pool, (Value *)format);
+
+    PrintfIns *printf_ins = (PrintfIns *)malloc(sizeof(PrintfIns));
+    printf_ins->tag = PRINTF_OP;
+    printf_ins->format = format_idx;
+    printf_ins->arity = printf_exp->nexps;
+    vector_add(info->funcContext->instructions, printf_ins);
+}
+
+static void compileMethodCall(CompileInfo *info, CallSlotExp *call) {
+    // 编译接收者对象
+    compileExpr(info, call->exp);
+
+    // 编译参数
+    for (int i = 0; i < call->nargs; i++) {
+        compileExpr(info, call->args[i]);
+    }
+
+    StringValue *name = newStringValue(strdup(call->name));
+    int name_idx = addConstantValue(info->pool, (Value *)name);
+
+    CallSlotIns *call_ins = (CallSlotIns *)malloc(sizeof(CallSlotIns));
+    call_ins->tag = CALL_SLOT_OP;
+    call_ins->name = name_idx;
+    call_ins->arity = call->nargs;
+    vector_add(info->funcContext->instructions, call_ins);
+}
+
+static void compileFunctionCall(CompileInfo *info, CallExp *call) {
+    for (int i = 0; i < call->nargs; i++) {
+        compileExpr(info, call->args[i]);
+    }
+
+    StringValue *name = newStringValue(strdup(call->name));
+    int name_idx = addConstantValue(info->pool, (Value *)name);
+
+    CallIns *call_ins = (CallIns *)malloc(sizeof(CallIns));
+    call_ins->tag = CALL_OP;
+    call_ins->name = name_idx;
+    call_ins->arity = call->nargs;
+    vector_add(info->funcContext->instructions, call_ins);
+}
+
+static void compileArray(CompileInfo *info, ArrayExp *array) {
+    compileExpr(info, array->length);
+    if (array->init != NULL) {
+        compileExpr(info, array->init);
+    }
+    ByteIns *new_array = (ByteIns *)malloc(sizeof(ByteIns));
+    new_array->tag = ARRAY_OP;
+    vector_add(info->funcContext->instructions, new_array);
+}
+
+static void compileSlotAccess(CompileInfo *info, SlotExp *slot) {
+    compileExpr(info, slot->exp);
+
+    StringValue *name = newStringValue(strdup(slot->name));
+    int name_idx = addConstantValue(info->pool, (Value *)name);
+
+    SlotIns *slot_ins = (SlotIns *)malloc(sizeof(SlotIns));
+    slot_ins->tag = SLOT_OP;
+    slot_ins->name = name_idx;
+    vector_add(info->funcContext->instructions, slot_ins);
+}
+
+static void compileSlotStmt(CompileInfo *info, SlotStmt *slot) {
+}
+
+static void compileObject(CompileInfo *info, ObjectExp *obj) {
+    if (obj->parent != NULL) {
+        compileExpr(info, obj->parent);
+    } else {
+        addNullInstr(info);
+    }
+
+    ObjContext *obj_ctx = newObjContext();
+    obj_ctx->prev = info->objContext;
+    info->objContext = obj_ctx;
+
+    for (int i = 0; i < obj->nslots; i++) {
+        compileSlotStmt(info, obj->slots[i]);
+    }
+
+    // 创建类对象
+    ClassValue *class_val = newClassValue(obj_ctx->slots);
+    int class_idx = addConstantValue(info->pool, (Value *)class_val);
+
+    // 生成NEW_OBJECT指令
+    // ObjectIns *new_obj = (ObjectIns *)malloc(sizeof(ObjectIns));
+    // new_obj->tag = OBJECT_OP;
+    // new_obj->idx = class_idx;
+    // vector_add(info->funcContext->instructions, new_obj);
+
+    // 恢复原对象上下文
+    info->objContext = obj_ctx->prev;
+    free(obj_ctx);
+}
+
+static void compileIfExpr(CompileInfo *info, IfExp *ifExp) {
+    StringValue *else_label = newStringValue(genLabel());
+    int else_idx = addConstantValue(info->pool, (Value *)else_label);
+    StringValue *end_label = newStringValue(genLabel());
+    int end_idx = addConstantValue(info->pool, (Value *)end_label);
+
+    compileExpr(info, ifExp->pred);
+
+    BranchIns *branch = (BranchIns *)malloc(sizeof(BranchIns));
+    branch->tag = BRANCH_OP;
+    branch->name = else_idx;
+    vector_add(info->funcContext->instructions, branch);
+
+    compileScope(info, ifExp->conseq);
+
+    GotoIns *goto_end = (GotoIns *)malloc(sizeof(GotoIns));
+    goto_end->tag = GOTO_OP;
+    goto_end->name = end_idx;
+    vector_add(info->funcContext->instructions, goto_end);
+
+    LabelIns *else_ins = (LabelIns *)malloc(sizeof(LabelIns));
+    else_ins->tag = LABEL_OP;
+    else_ins->name = else_idx;
+    vector_add(info->funcContext->instructions, else_ins);
+
+    if (ifExp->alt != NULL) {
+        compileScope(info, ifExp->alt);
+    }
+
+    LabelIns *end_ins = (LabelIns *)malloc(sizeof(LabelIns));
+    end_ins->tag = LABEL_OP;
+    end_ins->name = end_idx;
+    vector_add(info->funcContext->instructions, end_ins);
+}
+static void compileWhileExpr(CompileInfo *info, WhileExp *whileExp) {
+    StringValue *loop_label = newStringValue(genLabel());
+    int loop_idx = addConstantValue(info->pool, (Value *)loop_label);
+    StringValue *body_label = newStringValue(genLabel());
+    int body_idx = addConstantValue(info->pool, (Value *)body_label);
+
+    GotoIns *goto_loop = (GotoIns *)malloc(sizeof(GotoIns));
+    goto_loop->tag = GOTO_OP;
+    goto_loop->name = loop_idx;
+    vector_add(info->funcContext->instructions, goto_loop);
+
+    LabelIns *body_ins = (LabelIns *)malloc(sizeof(LabelIns));
+    body_ins->tag = LABEL_OP;
+    body_ins->name = body_idx;
+    vector_add(info->funcContext->instructions, body_ins);
+
+    compileScope(info, whileExp->body);
+
+    LabelIns *loop_ins = (LabelIns *)malloc(sizeof(LabelIns));
+    loop_ins->tag = LABEL_OP;
+    loop_ins->name = loop_idx;
+    vector_add(info->funcContext->instructions, loop_ins);
+
+    compileExpr(info, whileExp->pred);
+
+    BranchIns *branch = (BranchIns *)malloc(sizeof(BranchIns));
+    branch->tag = BRANCH_OP;
+    branch->name = body_idx;
+    vector_add(info->funcContext->instructions, branch);
+}
+
+static void compileRefExpr(CompileInfo *info, RefExp *ref) {
+    int local_idx = findLocalVar(info->funcContext, ref->name);
+    if (local_idx >= 0) {
+        GetLocalIns *get = (GetLocalIns *)malloc(sizeof(GetLocalIns));
+        get->tag = GET_LOCAL_OP;
+        get->idx = local_idx;
+        vector_add(info->funcContext->instructions, get);
+        return;
+    }
+
+    VarLocation loc = findObjVar(info->objContext, ref->name);
+    if (loc.index >= 0) {
+        if (loc.type == SLOT_VAR) {
+            SlotIns *get = (SlotIns *)malloc(sizeof(SlotIns));
+            get->tag = SLOT_OP;
+            get->name = loc.index;
+            vector_add(info->funcContext->instructions, get);
+        } else {
+            GetGlobalIns *get = (GetGlobalIns *)malloc(sizeof(GetGlobalIns));
+            get->tag = GET_GLOBAL_OP;
+            get->name = loc.index;
+            vector_add(info->funcContext->instructions, get);
+        }
+        return;
+    }
+
+    fprintf(stderr, "Error: Undefined variable '%s'\n", ref->name);
+    exit(1);
+}
+
+static void compileSetExpr(CompileInfo *info, SetExp *setExp) {
+    compileExpr(info, setExp->exp);
+
+    int local_idx = findLocalVar(info->funcContext, setExp->name);
+
+    if (local_idx >= 0) {
+        SetLocalIns *set_local = (SetLocalIns *)malloc(sizeof(SetLocalIns));
+        set_local->tag = SET_LOCAL_OP;
+        set_local->idx = local_idx;
+        vector_add(info->funcContext->instructions, set_local);
+    } else {
+        VarLocation loc = findObjVar(info->objContext, setExp->name);
+
+        if (loc.index >= 0) {
+            if (loc.type == GLOBAL_VAR) {
+                SetGlobalIns *set_global = (SetGlobalIns *)malloc(sizeof(SetGlobalIns));
+                set_global->tag = SET_GLOBAL_OP;
+                set_global->name = loc.index;
+                vector_add(info->funcContext->instructions, set_global);
+            } else { // SLOT_VAR
+                SetSlotIns *set_slot = (SetSlotIns *)malloc(sizeof(SetSlotIns));
+                set_slot->tag = SET_SLOT_OP;
+                set_slot->name = loc.index;
+                vector_add(info->funcContext->instructions, set_slot);
+            }
+        } else {
+            fprintf(stderr, "Error: Undefined variable '%s' in assignment\n", setExp->name);
+            exit(1);
+        }
+    }
+}
+
 static void compileExpr(CompileInfo *info, Exp *expr) {
+    switch (expr->tag) {
+    case NULL_EXP: {
+        Value *null_val = newNullValue();
+        compileLiteral(info, null_val);
+        break;
+    }
+    case INT_EXP: {
+        IntExp *int_exp = (IntExp *)expr;
+        IntValue *int_val = newIntValue(int_exp->value);
+        compileLiteral(info, (Value *)int_val);
+        break;
+    }
+    case ARRAY_EXP:
+        compileArray(info, (ArrayExp *)expr);
+        break;
+    case PRINTF_EXP:
+        compilePrintf(info, (PrintfExp *)expr);
+        break;
+    case OBJECT_EXP:
+        // compileObject(info, (ObjectExp *)expr);
+        break;
+    case SLOT_EXP:
+        // compileSlotAccess(info, (SlotExp *)expr);
+        break;
+    case SET_SLOT_EXP:
+        // compileSetSlot(info, (SetSlotExp *)expr);
+        break;
+    case CALL_SLOT_EXP:
+        // compileMethodCall(info, (CallSlotExp *)expr);
+        break;
+    case CALL_EXP:
+        compileFunctionCall(info, (CallExp *)expr);
+        break;
+    case SET_EXP:
+        compileSetExpr(info, (SetExp *)expr);
+        break;
+    case IF_EXP:
+        compileIfExpr(info, (IfExp *)expr);
+        break;
+    case WHILE_EXP:
+        compileWhileExpr(info, (WhileExp *)expr);
+        break;
+    case REF_EXP:
+        compileRefExpr(info, (RefExp *)expr);
+        break;
+    }
 }
 
 static void compileVarStmt(CompileInfo *info, ScopeVar *varStmt) {
@@ -207,6 +556,7 @@ static void compileFnStmt(CompileInfo *info, ScopeFn *fnStmt) {
     info->funcContext = fn_ctx;
 
     compileScope(info, fnStmt->body);
+    addReturnInstr(info);
 
     MethodValue *method = newMethodValue(
         name_idx,
@@ -244,16 +594,18 @@ static void compileScope(CompileInfo *info, ScopeStmt *stmt) {
 
     case EXP_STMT:
         compileExpr(info, ((ScopeExp *)stmt)->exp);
-        // Add DROP instruction since expression result is not used
-        ByteIns *drop = (ByteIns *)malloc(sizeof(ByteIns));
-        drop->tag = DROP_OP;
-        vector_add(info->funcContext->instructions, drop);
         break;
 
     default:
         fprintf(stderr, "Unknown statement type: %d\n", stmt->tag);
         break;
     }
+}
+
+void interpret_bc(Program *p) {
+    printf("Interpreting Bytecode Program:\n");
+    print_prog(p);
+    printf("\n");
 }
 
 Program *compile(ScopeStmt *stmt) {
@@ -269,6 +621,10 @@ Program *compile(ScopeStmt *stmt) {
     // Compile program from global level
     compileScope(info, stmt);
 
+    // Add null and return to entry function
+    addNullInstr(info);
+    addReturnInstr(info);
+
     // Add global context's binding function as new slot
     char *str = (char *)malloc(strlen("42entry24") + 1);
     strcpy(str, "42entry24");
@@ -279,5 +635,9 @@ Program *compile(ScopeStmt *stmt) {
     prog->entry = entryIndex;
     prog->values = info->pool;
     prog->slots = info->objContext->slots;
+
+#ifdef DEBUG
+    interpret_bc(prog);
+#endif
     return prog;
 }
