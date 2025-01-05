f9f41fc2af43e0854f187710f94411501eacf27e
HaleOIC <shinehale730@gmail.com>
2025-01-05 21:41:31 +0100

    feat: add custom strdup implementation if not defined

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,16 @@
 #include "feeny/utils.h"
 #include "feeny/collector.h"
 
+#ifndef strdup
+char *strdup(const char *s) {
+    size_t len = strlen(s) + 1;
+    char *new = malloc(len);
+    if (new == NULL)
+        return NULL;
+    return (char *)memcpy(new, s, len);
+}
+#endif
+
 //============================================================
 //===================== CONVENIENCE ==========================
 //============================================================
